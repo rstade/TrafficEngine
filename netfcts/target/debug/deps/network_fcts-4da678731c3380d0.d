3fe16d2dd7c0571c2eac4d2e2fb2ce2e47b1e4c3
Rainer Stademann <rainer@spacebandit.de>
2018-11-18 16:28:36 +0100

    removed spurious directory netfcts

--- netfcts/target/debug/deps/network_fcts-4da678731c3380d0.d
+++
@@ -1,5 +0,0 @@
-/home/rainer/work/trafficengine/network_fcts/target/debug/deps/network_fcts-4da678731c3380d0: src/lib.rs
-
-/home/rainer/work/trafficengine/network_fcts/target/debug/deps/network_fcts-4da678731c3380d0.d: src/lib.rs
-
-src/lib.rs:
--- netfcts/target/debug/network_fcts-4da678731c3380d0.d
+++
@@ -1,1 +0,0 @@
-/home/rainer/work/trafficengine/network_fcts/target/debug/network_fcts-4da678731c3380d0: /home/rainer/work/trafficengine/network_fcts/src/lib.rs
